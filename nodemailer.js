4cedcc24361ccc0eae6ced6727e4e519411ad898
Igboanugwo Collins <munisco12@gmail.com>
2021-10-17 23:16:53 +0100

    Added email registration success to signup

--- nodemailer.js
+++ nodemailer.js
@@ -1,22 +1,20 @@
 const nodeMailer = require('nodemailer')
 
-exports.transporter = function() {
-    console.log('nodemailer connected')
-    nodeMailer
-        .createTransport({
-            host: 'mail.growveonct.com',
-            port: 587,
-            secure: false,
-            requireTLS: true,
-            socketTimeout: 1200000,
-            connectionTimeout: 1200000,
-            auth: {
-                user: 'admin@growveonct.com',
-                pass: 'Panther1.?)0',
-            },
-            tls: {
-                rejectUnauthorized: false,
-            },
-        })
-        
+exports.transporter = function () {
+
+    return nodeMailer.createTransport({
+        host: 'mail.growveonct.com',
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        socketTimeout: 1200000,
+        connectionTimeout: 1200000,
+        auth: {
+            user: 'admin@growveonct.com',
+            pass: 'Panther1.?)0',
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    })
 }
