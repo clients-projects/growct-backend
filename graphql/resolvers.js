0b26c241452a5c57e026cf838674ac311e689b59
Igboanugwo Collins <munisco12@gmail.com>
2021-10-18 21:01:11 +0100

    Added deposit approval email

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -739,17 +739,21 @@ module.exports = {
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
   <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
 
   <style type="text/css">
    
       body,
       p,
       div {
-      font-family: arial, helvetica, sans-serif;
+      font-family: 'Poppins', helvetica, sans-serif;
       }
 
     body {
       color: #000000;
+      font-weight: 500;
     }
 
     body a {
