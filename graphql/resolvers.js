23ee75481800f1dbbe0520ff642c4ffa608b9153
Igboanugwo Collins <munisco12@gmail.com>
2021-10-20 09:01:10 +0100

    Added withdraw email

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -264,7 +264,6 @@ module.exports = {
                         console.log({ err })
                     })
 
-                console.log('sent email...')
                 if (createdUser) {
                     return {
                         ...createdUser._doc,
@@ -663,6 +662,161 @@ module.exports = {
             user.pendingWithdrawals.push(savePendingWithdrawNow)
 
             await user.save()
+
+             const transporter = nodeMailer.createTransport({
+                 host: 'mail.growveonct.com',
+                 port: 465,
+                 secure: true,
+                 requireTLS: true,
+                 socketTimeout: 1200000,
+                 connectionTimeout: 1200000,
+                 auth: {
+                     user: 'admin@growveonct.com',
+                     pass: 'Panther1.?)0',
+                 },
+                 tls: {
+                     rejectUnauthorized: false,
+                 },
+             })
+
+             const mailOptions = {
+                 from: '"Admin in growveon" <admin@growveonct.com>',
+                 to: userData.email,
+                 subject: 'Welcome to Growveon Crypto Trading',
+                 html: `  <head>
+                                    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+                                    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+
+                                    <style type="text/css">
+                                    body, p, div {
+                                    font-family: arial,helvetica,sans-serif;
+                                    font-size: 14px;
+                                    }
+                                    body {
+                                    color: #000000;
+                                    }
+                                    body a {
+                                    color: #1188E6;
+                                    text-decoration: none;
+                                    }
+                                    p { margin: 0; padding: 0; }
+                                    table.wrapper {
+                                    width:100% !important;
+                                    table-layout: fixed;
+                                    -webkit-font-smoothing: antialiased;
+                                    -webkit-text-size-adjust: 100%;
+                                    -moz-text-size-adjust: 100%;
+                                    -ms-text-size-adjust: 100%;
+                                    }
+                                    img.max-width {
+                                    max-width: 100% !important;
+                                    }
+                                    .column.of-2 {
+                                    width: 50%;
+                                    }
+                                    .column.of-3 {
+                                    width: 33.333%;
+                                    }
+                                    .column.of-4 {
+                                    width: 25%;
+                                    }
+                                    ul ul ul ul  {
+                                    list-style-type: disc !important;
+                                    }
+                                    ol ol {
+                                    list-style-type: lower-roman !important;
+                                    }
+                                    ol ol ol {
+                                    list-style-type: lower-latin !important;
+                                    }
+                                    ol ol ol ol {
+                                    list-style-type: decimal !important;
+                                    }
+                                   
+                                </style>
+                                    </head>
+                                    <body>
+                                    <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
+                                        <div class="webkit">
+                                        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+                                            <tr>
+                                            <td valign="top" bgcolor="#FFFFFF" width="100%">
+                                                <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                                                <tr>
+                                                    <td width="100%">
+                                                    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                                                        <tr>
+                                                        <td>
+
+                                                                    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                                                    <tr>
+                                                                        <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+                                    <tr>
+                                    <td role="module-content">
+                                        <p>growveonct</p>
+                                    </td>
+                                    </tr>
+                                </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="36afb5b3-dfbf-4d2c-9c03-996ad2a46c9d">
+                                    <tbody>
+                                    <tr>
+                                        <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+                                        <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:16% !important; width:16%; height:auto !important;" width="96" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/908c8197705a575b/25ab05d9-1e7b-4bdb-9207-079957416ad0/253x223.png">
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="fa5da507-cc33-436d-b2ac-39fc60376d63">
+                                    <tbody>
+                                    <tr>
+                                        <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table><table class="module" role="module" data-type="code" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="c3cd393f-be24-4fb9-851e-1be32af6971f">
+                                    <tbody>
+                                    <tr>
+                                        <td height="100%" valign="top" role="module-content"><h2 style="text-align: center; font-family: inherit">Welcome to Growveon Crypto Trading</h2>
+                                <div style="font-family: inherit; padding: 1rem 0">Hi ${userData.username},</div>
+                                <p style="font-family: inherit; margin-bottom: 4rem">We are delighted to have you on this platform, It's awesome to see you take a strategic approach towards your financial growth.</p>
+                                <div style="font-family: inherit; text-align: left"><a href="https://growveonct.com/pricing">Check out our packages&nbsp;</a></div></td>
+                                    </tr>
+                                    </tbody>
+                                </table><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="27fcb3e3-1d36-4f3c-9e3a-7e890d155009">
+                                    <tbody>
+                                    <tr>
+                                        <td style="padding:0px 0px 70px 0px;" role="module-content" bgcolor="">
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#086aa8; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-family:helvetica,sans-serif; font-size:12px; line-height:20px;"><a target="_blank" class="Unsubscribe--unsubscribeLink zzzzzzz" href="https://growveonct.com" style="color:#0820b6;">growveon crypto trading</a></p></div></td>
+                                                                    </tr>
+                                                                    </table>
+
+                                                        </td>
+                                                        </tr>
+                                                    </table>
+                                                    </td>
+                                                </tr>
+                                                </table>
+                                            </td>
+                                            </tr>
+                                        </table>
+                                        </div>
+                                    </center>
+                                    </body>
+                `,
+             }
+
+
+             transporter
+                 .sendMail(mailOptions)
+                 .then((res) => {
+                     console.log({ res })
+                 })
+                 .catch((err) => {
+                     console.log({ err })
+                 })
+
 
             return {
                 ...savePendingWithdrawNow._doc,
